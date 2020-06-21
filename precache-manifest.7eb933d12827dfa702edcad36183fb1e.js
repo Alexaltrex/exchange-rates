d5fdd4ad82ef197bb3b675e1417f24ab62039418
Alexaltrex <alexaltrex@mail.ru>
2020-06-22 01:39:38 +0400

    Updates

---
+++ precache-manifest.7eb933d12827dfa702edcad36183fb1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfb3a2642e56983ccba51a35ea7d750",
+    "url": "/exchange-rates/index.html"
+  },
+  {
+    "revision": "585d96b324465176938a",
+    "url": "/exchange-rates/static/css/main.f697f577.chunk.css"
+  },
+  {
+    "revision": "e591650b12c7a8440571",
+    "url": "/exchange-rates/static/js/2.20ad241a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/exchange-rates/static/js/2.20ad241a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "585d96b324465176938a",
+    "url": "/exchange-rates/static/js/main.aed8a29e.chunk.js"
+  },
+  {
+    "revision": "c97f4def052fcbf6077d",
+    "url": "/exchange-rates/static/js/runtime-main.f42318c0.js"
+  },
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "/exchange-rates/static/media/oval.be00fc4a.svg"
+  }
+]);
